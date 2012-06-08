842bb5acb456203ddca8b15f3364d506c45ea69a
Vinicius Braga <contato@viniciusbraga.com>
2012-06-08 17:49:50 -0300

    Criei o primeiro teste.

--- src/core/tests.py
+++ src/core/tests.py
@@ -8,9 +8,9 @@ Replace this with more appropriate tests for your application.
 from django.test import TestCase
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class HomepageTest(TestCase):
+    def test_get_homepage(self):
+        response = self.client.get('/')
+        self.assertEquals(200, response.status_code)
+        self.assertTemplateUsed(response, 'index.html')
+
