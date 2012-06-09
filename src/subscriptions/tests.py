97b15ef8bda830f39959c7db340b2346e1db2129
Vinicius Braga <contato@viniciusbraga.com>
2012-06-09 20:50:45 -0300

    Separando os testes de vier dos testes de URl

--- src/subscriptions/tests.py
+++ src/subscriptions/tests.py
@@ -4,14 +4,19 @@ from django.test import TestCase
 from django.core.urlresolvers import reverse as r
 
 
-class SubscriptionTest(TestCase):
-    def test_get_url_subscribe(self):
+class SubscriptionUlrTest(TestCase):
+
+    def test_get_subscribe_instance(self):
         response = self.client.get( r('subscriptions:subscribe') )
         self.assertEquals(200, response.status_code)
 
-    def test_get_url_success(self):
+    def test_get_success_instance(self):
         response = self.client.get( r('subscriptions:success', args=[1]) )
         self.assertEquals(200, response.status_code)
+
+
+
+class SubscribeViewTest(TestCase):
 
     def test_get_view_success(self):
         "Ao visitar /inscricao/ a página de inscrição é exibida."
@@ -22,5 +27,4 @@ class SubscriptionTest(TestCase):
         "Não encontrou o template da inscrição."
         response = self.client.get( r('subscriptions:subscribe') )
         self.assertTemplateUsed(response, 'subscriptions/subscription_form.html')
-
 
