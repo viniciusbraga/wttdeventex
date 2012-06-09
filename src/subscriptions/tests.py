8b841e945ea6d75b0ca4bbfe2329ad5db1021ca9
Vinicius Braga <contato@viniciusbraga.com>
2012-06-09 17:06:04 -0300

    Refatora os testes e aplica aquele "r" esquisito… #EuNuncaUsoIsto

--- src/subscriptions/tests.py
+++ src/subscriptions/tests.py
@@ -1,13 +1,14 @@
 # coding: utf-8
 
 from django.test import TestCase
+from django.core.urlresolvers import reverse as r
 
 
 class SubscriptionTest(TestCase):
     def test_get_subscribe_instance(self):
-        response = self.client.get('/inscricao/')
+        response = self.client.get( r('subscriptions:subscribe') )
         self.assertEquals(200, response.status_code)
 
     def test_get_success_instance(self):
-        response = self.client.get('/inscricao/1/')
+        response = self.client.get( r('subscriptions:success', args=[1]) )
         self.assertEquals(200, response.status_code)
