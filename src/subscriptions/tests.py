2d9666442448017718003fbf35f71850ec6e0b38
Vinicius Braga <contato@viniciusbraga.com>
2012-06-09 20:53:55 -0300

    Refatora os testes de View

--- src/subscriptions/tests.py
+++ src/subscriptions/tests.py
@@ -17,14 +17,14 @@ class SubscriptionUlrTest(TestCase):
 
 
 class SubscribeViewTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get(r('subscriptions:subscribe'))
 
     def test_get_view_success(self):
         "Ao visitar /inscricao/ a página de inscrição é exibida."
-        response = self.client.get( r('subscriptions:subscribe') )
-        self.assertEquals(200, response.status_code)
+        self.assertEquals(200, self.resp.status_code)
 
     def test_use_template_subscribe(self):
         "Não encontrou o template da inscrição."
-        response = self.client.get( r('subscriptions:subscribe') )
-        self.assertTemplateUsed(response, 'subscriptions/subscription_form.html')
+        self.assertTemplateUsed(self.resp, 'subscriptions/subscription_form.html')
 
