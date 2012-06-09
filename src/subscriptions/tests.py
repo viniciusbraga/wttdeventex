b366a7c8af056ea7d8dee5c4330b0a058fa25231
Vinicius Braga <contato@viniciusbraga.com>
2012-06-09 18:08:19 -0300

    Melhora a nomeação das testes

--- src/subscriptions/tests.py
+++ src/subscriptions/tests.py
@@ -5,10 +5,10 @@ from django.core.urlresolvers import reverse as r
 
 
 class SubscriptionTest(TestCase):
-    def test_get_subscribe_instance(self):
+    def test_get_url_subscribe(self):
         response = self.client.get( r('subscriptions:subscribe') )
         self.assertEquals(200, response.status_code)
 
-    def test_get_success_instance(self):
+    def test_get_url_success(self):
         response = self.client.get( r('subscriptions:success', args=[1]) )
         self.assertEquals(200, response.status_code)
