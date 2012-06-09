90a4a6aceda7c514080a77257e5c1cc49a2bc899
Vinicius Braga <contato@viniciusbraga.com>
2012-06-09 18:23:34 -0300

    Cria os testes para a vier e template do Subscribe

--- src/subscriptions/tests.py
+++ src/subscriptions/tests.py
@@ -12,3 +12,15 @@ class SubscriptionTest(TestCase):
     def test_get_url_success(self):
         response = self.client.get( r('subscriptions:success', args=[1]) )
         self.assertEquals(200, response.status_code)
+
+    def test_get_view_success(self):
+        "Ao visitar /inscricao/ a página de inscrição é exibida."
+        response = self.client.get( r('subscriptions:subscribe') )
+        self.assertEquals(200, response.status_code)
+
+    def test_use_template_subscribe(self):
+        "Não encontrou o template da inscrição."
+        response = self.client.get( r('subscriptions:subscribe') )
+        self.assertTemplateUsed(response, 'subscriptions/subscription_form.html')
+
+
