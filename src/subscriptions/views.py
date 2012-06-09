dec646abbeedc23e8b3bfa56d4fc115a179d88b3
Vinicius Braga <contato@viniciusbraga.com>
2012-06-09 18:24:15 -0300

    Acrescenta o template do form

--- src/subscriptions/views.py
+++ src/subscriptions/views.py
@@ -1,9 +1,10 @@
 # coding: utf-8
 
 from django.http import HttpResponse
+from django.views.generic.simple import direct_to_template
 
 def subscribe(request):
-	return HttpResponse()
+	return direct_to_template(request, template='subscriptions/subscription_form.html')
 
 def success(request, pk):
 	return HttpResponse()
